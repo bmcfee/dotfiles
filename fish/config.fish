5d168eba4fe32d67a386db8bf2585e9001b32ff0
Brian McFee <brian.mcfee@nyu.edu>
2025-06-09 10:46:17 -0400

    fixers uppers

--- fish/config.fish
+++ fish/config.fish
@@ -1,28 +1,18 @@
 set fish_greeting ""
-#set FLINE_PATH $HOME/.config/fisherman/fishline
 
 if set -q SSH_CLIENT
     set USERHOST USERHOST
 else
     set USERHOST 
 end
-#set FLINE_PROMPT STATUS $USERHOST SCREEN JOBS CONDA GIT FULLPWD WRITE N ROOT SPACE
-#fisher install IlanCosman/tide@v5
-#
-#set -gx PATH $HOME/.gem/ruby/2.5.0/bin $HOME/miniconda3/bin $PATH
 set -x PKG_CONFIG_PATH $HOME/lib/pkgconfig
 
 set -x MAMBA_NO_BANNER 1
 
-# source $HOME/.config/fish/condaline.fish
 #source $HOME/.config/fish/conf.d/dracula.fish
 #source $HOME/.config/fish/conf.d/bauhaus.fish
 source $HOME/.config/fish/themes/tokyonight_moon.fish
 #source $FLINE_PATH/themes/washed.fish
-
-#function fish_prompt
-#    fishline $status
-#end
 
 alias top=btop
 alias vim=nvim
@@ -44,8 +34,9 @@ alias cda="conda deactivate"
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /home/bmcfee/miniconda3/bin/conda
-    status is-interactive && eval /home/bmcfee/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-    status is-interactive && cda
+    #    status is-interactive && eval /home/bmcfee/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    #    status is-interactive && cda
     status is-interactive && ca py13
 end
 # <<< conda initialize <<<
+#
